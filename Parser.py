9c89c89342c71b3463c3a4bed64679e0a6072992
dgliga238 <daniela.gliga@isa.utm.md>
2024-10-10 10:51:40 +0300

    scrape teh link

--- Parser.py
+++ Parser.py
@@ -10,9 +10,22 @@ if response.status_code == 200:
 
     for product in soup.find_all("figure", class_="card card-product border-0"):
 
-        names = product.find("figcaption").find("div", class_="title").a.get_text(strip=True)
-        prices = product.find("div", class_="price-wrap h5 d-flex flex-column m-0").span.get_text(strip=True)
-        print(f"{names}: {prices}")
+        names = product.find("figcaption").find("div", class_="title").a.text
+        prices = product.find("div", class_="price-wrap h5 d-flex flex-column m-0").span.text
+        link = product.find("figcaption").find("div", class_="title").a.get("href")
+
+        response2 = requests.get(link)
+        if response2.status_code == 200:
+            soup2 = BeautifulSoup(response2.text, "html.parser")
+
+            prod_elements = soup2.find_all("td", class_="p-8 p-xxl-16")
+
+            if len(prod_elements) >= 2:
+                producator = prod_elements[1].text
+                print(f"{names} Price: {prices}\n Producator: {producator} ")
+
+        else:
+            print("error")
 
 else:
     print(f"Request failed with status code: {response.status_code}")
